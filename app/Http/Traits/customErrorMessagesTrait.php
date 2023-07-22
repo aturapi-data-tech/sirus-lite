367af2e0eec3ab1cdb456e2c9aa76f5243e69ac2
nwanshary <nuurwahidanshary@gmail.com>
2023-07-22 18:26:47 +0700

    Pendaftaran Pasien V1.2.10

--- app/Http/Traits/customErrorMessagesTrait.php
+++ app/Http/Traits/customErrorMessagesTrait.php
@@ -11,7 +11,7 @@ trait customErrorMessagesTrait
             'required' => 'Data tidak boleh kosong.', //data tdak boleh kosong
             'digits' => 'Data harus berisi :digits digit.', //untuk angka harus berisi a length
             'digits_between' => 'Data harus berisi antara :min-:max digit.', //untuk angka  antara a,b length
-            'min' => ':attribute Data berisi minimal :min digit.', //untuk angka /character min length
+            'min' => 'Data berisi minimal :min digit.', //untuk angka /character min length
             'max' => 'Data berisi maximl :max digit.', //untuk angka /character max length
             'exists' => 'Data tidak ada didalam data master', // mencari referensi pada table tertentu
             'date_format' => 'Format tgl dd/mm/yyyy' //format tgl dd/mm/yyyy
