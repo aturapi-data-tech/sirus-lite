1f36f1c44d4fccc0fc0c20436ff782240b4229b0
nwanshary <nuurwahidanshary@gmail.com>
2023-12-28 10:26:18 +0700

    fix(emr-u-h-d):blade UGD Rawat Jalan

--- resources/views/livewire/emr-u-g-d/emr-u-g-d.blade.php
+++ resources/views/livewire/emr-u-g-d/emr-u-g-d.blade.php
@@ -303,12 +303,6 @@
                                                         </x-dropdown-link>
                                                     </li>
 
-                                                    <li>
-                                                        <x-dropdown-link
-                                                            wire:click="edit('{{ $myQData->rj_no }}','{{ $myQData->reg_no }}')">
-                                                            {{ __('UGD Rawat Inap') }}
-                                                        </x-dropdown-link>
-                                                    </li>
                                                     {{-- <li>
                                                         <x-dropdown-link
                                                             wire:click="$emit('confirm_remove_record', '{{ $myQData->rj_no }}', '{{ $myQData->reg_name }}')">
