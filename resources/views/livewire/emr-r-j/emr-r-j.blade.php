c25ea1a03861762ba1d0d9ff112b9f0c50b6c4d2
nwanshary <nuurwahidanshary@gmail.com>
2024-01-25 08:08:34 +0700

    fix(emr-r-j) Blade message option pindah ke kiri atas

--- resources/views/livewire/emr-r-j/emr-r-j.blade.php
+++ resources/views/livewire/emr-r-j/emr-r-j.blade.php
@@ -409,6 +409,24 @@
 
         {{-- Global Livewire JavaScript Object start --}}
         <script type="text/javascript">
+            toastr.options = {
+                "closeButton": false,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": false,
+                "positionClass": "toast-top-left",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            }
+
             window.livewire.on('toastr-success', message => toastr.success(message));
             window.Livewire.on('toastr-info', (message) => {
                 toastr.info(message)
