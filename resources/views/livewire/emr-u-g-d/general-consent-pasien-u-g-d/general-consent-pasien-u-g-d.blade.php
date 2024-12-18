a8ea116a52c4ac666dec8c635b740a2afb3fe577
SYAMS. info <nuurwahidanshary@gmail.com>
2024-12-18 08:21:25 +0700

    fix(general-conset):tambahkan keterangan

--- resources/views/livewire/emr-u-g-d/general-consent-pasien-u-g-d/general-consent-pasien-u-g-d.blade.php
+++ resources/views/livewire/emr-u-g-d/general-consent-pasien-u-g-d/general-consent-pasien-u-g-d.blade.php
@@ -5,125 +5,240 @@
         <div class="w-full p-4 text-sm ">
             <h2 class="text-2xl font-bold text-center">Formulir Persetujuan Umum</h2>
             </br>
-            <p class="font-bold">Saya yang bertanda tangan di bawah ini menyatakan dengan sesungguhnya bahwa:
-            </p>
 
-            <div id="hakPasien">
-                <p>
-                    <span class="font-bold">
-                        HAK DAN KEWAJIBAN SEBAGAI PASIEN:
-                    </span>
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        1.
-                    </span>
-                    Memperoleh informasi mengenai tata tertib dan peraturan yang berlaku di rumah sakit
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        2.
-                    </span>
-                    Memperoleh informasi tentang hak dan kewajiban pasien
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        3.
-                    </span>
-                    Memperoleh layanan yang menusiawi, adil,
-                    jujur dan tanpa diskriminasi
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        4.
-                    </span>
-                    Memperoleh layanan kesehatan yang bermutu sesuai dengan standar profesi dan standar prosedur
-                    operasional
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        5.
-                    </span>
-                    Memperoleh layanan yang efektif dan efisien sehingga pasien terhindar dari kerugian fisik dan materi
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        6.
-                    </span>
-                    Mengajukan pengaduan atas kualitas pelayanan yang didapatkan
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        7.
-                    </span>
-                    Memilih dokter dan kelas perawatan sesuai dengan keinginannya dan peraturan yang berlaku di rumah
-                    sakit
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        8.
-                    </span>
-                    Meminta konsultasi tentang penvakit yang dideritanya kepada dokter lain yang mempunyai surat izin
-                    praktik (sip) baik di dalam maupun di luar rumah sakit
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        9.
-                    </span>
-                    Mendapatkan privasi dan kerahasiaan penyakit yang diderita termasuk data-data medisnya
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        10.
-                    </span>
-                    Mendapat informasi yang meliputi diagnosis dan tata cara tindakan medis, tujuan tindakan medis,
-                    alternatif tindakan, risiko dan komplikasi yang mungkin terjadi, dan prognosis terhadap tindakan
-                    yang dilakukan serta perkiraan biaya pengobatan.
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        11.
-                    </span>
-                    Memberikan persetujuan atau menolak atas tindakan
-                    yang akan dilakukan oleh tenaga kesehatan terhadap penyakit yanng dideritanya
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        12.
-                    </span>
-                    Didampingi keluarganya dalam keadaan kritis
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        13.
-                    </span>
-                    Menjalankan ibadah sesuai agama atau kepercayaan yang dianutnya selama hal itu tidak mengganggu
-                    pasien lainnya
-                </p>
-                </br>
-                <p>
-                    <span class="font-bold">
-                        14.
-                    </span>
-                    Memperoleh keamanan dan keselamatan dirinya selama dalam perawatan di rumah sakit
-                </p>
-                </br>
+            <div class="grid grid-cols-2 gap-2">
+                <div id="hakPasien">
+                    <p>
+                        <span class="font-bold">
+                            HAK SEBAGAI PASIEN:
+                        </span>
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            1.
+                        </span>
+                        Memperoleh informasi mengenai tata tertib dan peraturan yang berlaku di rumah sakit
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            2.
+                        </span>
+                        Memperoleh informasi tentang hak dan kewajiban pasien
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            3.
+                        </span>
+                        Memperoleh layanan yang menusiawi, adil,
+                        jujur dan tanpa diskriminasi
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            4.
+                        </span>
+                        Memperoleh layanan kesehatan yang bermutu sesuai dengan standar profesi dan standar prosedur
+                        operasional
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            5.
+                        </span>
+                        Memperoleh layanan yang efektif dan efisien sehingga pasien terhindar dari kerugian fisik dan
+                        materi
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            6.
+                        </span>
+                        Mengajukan pengaduan atas kualitas pelayanan yang didapatkan
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            7.
+                        </span>
+                        Memilih dokter dan kelas perawatan sesuai dengan keinginannya dan peraturan yang berlaku di
+                        rumah
+                        sakit
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            8.
+                        </span>
+                        Meminta konsultasi tentang penvakit yang dideritanya kepada dokter lain yang mempunyai surat
+                        izin
+                        praktik (sip) baik di dalam maupun di luar rumah sakit
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            9.
+                        </span>
+                        Mendapatkan privasi dan kerahasiaan penyakit yang diderita termasuk data-data medisnya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            10.
+                        </span>
+                        Mendapat informasi yang meliputi diagnosis dan tata cara tindakan medis, tujuan tindakan medis,
+                        alternatif tindakan, risiko dan komplikasi yang mungkin terjadi, dan prognosis terhadap tindakan
+                        yang dilakukan serta perkiraan biaya pengobatan.
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            11.
+                        </span>
+                        Memberikan persetujuan atau menolak atas tindakan
+                        yang akan dilakukan oleh tenaga kesehatan terhadap penyakit yanng dideritanya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            12.
+                        </span>
+                        Didampingi keluarganya dalam keadaan kritis
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            13.
+                        </span>
+                        Menjalankan ibadah sesuai agama atau kepercayaan yang dianutnya selama hal itu tidak mengganggu
+                        pasien lainnya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            14.
+                        </span>
+                        Memperoleh keamanan dan keselamatan dirinya selama dalam perawatan di rumah sakit
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            15.
+                        </span>
+                        Mengajukan usul, saran, perbaikan atas perlakuan rumah sakit terhadap dirinya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            16.
+                        </span>
+                        Menolak pelayanan bimbingan rohani yang tidak sesuai dengan agama dan kepercayaan yang dianutnya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            17.
+                        </span>
+                        Menggugat dan/atau menuntut rumah sakit apabila rumah sakit diduga memberikan yang tidak sesuai
+                        dengan standar baik secara perdata ataupun pidana
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            18.
+                        </span>
+                        Mengeluhkan pelayanan rumah sakit yang tidak sesuai dengan standar pelayanan melalui media cetak
+                        dan
+                        elektronik sesuai dengan ketentuan peraturan perundang-undangan
+                    </p>
+                    </br>
+                </div>
+
+                <div id="KEWAJIBANPasien">
+                    <p>
+                        <span class="font-bold">
+                            KEAWAJIBAN SEBAGAI PASIEN:
+                        </span>
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            1.
+                        </span>
+                        Mematuhi peraturan yang berlaku di rumah sak
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            2.
+                        </span>
+                        Menggunakan fasilitas rumah sakit secara bertanggungjawab,
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            3.
+                        </span>
+                        Menghormati hak-hak pasien lain, pengunjung dan hak tenaga kesehatan serta petugas lainya yang
+                        bekerja di rumah sakit
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            4.
+                        </span>
+                        Memberikan informasi yang jujur, lengkap dan akurat sesuai kemampuan dan pengetahuannya tentang
+                        masalah kesehatannya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            5.
+                        </span>
+                        Memberikan informasi mengenai kemampuan finansial dan jaminan kesehatan yang dimilikinya
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            6.
+                        </span>
+                        Mematuhi rencana terapi yang direkomendasikan oleh tenaga kesehatan di rumah sakit dan di
+                        setujui
+                        oleh pasien yang bersangkutan setelah mendapatkan penjelasan sesuai ketentuan peraturan
+                        perundang-undangan
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            7.
+                        </span>
+                        Menerima segala konsekuensi atas keputusan pribadinya untuk menolak rencana terapi yang
+                        direkomendasikan oleh tenaga kesehatan dan/atau tidak mematuhi petunjuk yang diberikan oleh
+                        tenaga
+                        kesehatan dalam rangka penyembuhan penyakit atau masalah kesehatannya: dan Memberikan imbalan
+                        jasa
+                        atas pelayanan yang diterima
+                    </p>
+                    </br>
+                    <p>
+                        <span class="font-bold">
+                            8.
+                        </span>
+                        PERSETUJUAN PELAYANAN KESEHATAN. Saya menyetujui dan memberikan persetujuan untuk mendapat
+                        pelayanan
+                        kesehatan
+                    </p>
+                    </br>
+                </div>
             </div>
-
-            <div id="persetujuan">
+            <x-theme-line></x-theme-line>
+            <div id="persetujuan" class="mt-4">
+                <p class="font-bold">
+                    Saya yang bertanda tangan di bawah ini menyatakan dengan sesungguhnya bahwa:
+                </p>
+                </br>
                 <p>
                     <span class="font-bold">
                         Pemahaman:
@@ -233,7 +348,7 @@
             @else
                 <div class="w-full bg-white ">
 
-                    <div class="w-64 h-40 ">
+                    <div class="w-56 h-auto">
                         <div class="text-sm text-right ">
                             {{ env('SATUSEHAT_ORGANIZATION_NAMEX', 'RUMAH SAKIT ISLAM MADINAH') }}
                         </div>
