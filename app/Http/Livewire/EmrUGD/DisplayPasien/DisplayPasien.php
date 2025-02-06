9dea358581a2d8b597d41d02ecb78293c5019d65
SYAMS. info <nuurwahidanshary@gmail.com>
2025-02-06 19:36:22 +0700

    fix(DisplaPasien):tambahkan listener to mount dan ubah find data menjadi trait

--- app/Http/Livewire/EmrUGD/DisplayPasien/DisplayPasien.php
+++ app/Http/Livewire/EmrUGD/DisplayPasien/DisplayPasien.php
@@ -5,12 +5,15 @@ namespace App\Http\Livewire\EmrUGD\DisplayPasien;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Http\Traits\EmrUGD\EmrUGDTrait;
 
 
 class DisplayPasien extends Component
 {
+    use EmrUGDTrait;
+
     protected $listeners = [
-        'syncronizeAssessmentPerawatRJFindData' => 'mount'
+        'syncronizeAssessmentPerawatUGDFindData' => 'mount'
     ];
 
     // public function listenerRegNo($newRegNo): void
@@ -101,159 +104,8 @@ class DisplayPasien extends Component
 
     private function findData($rjno): void
     {
-
-
-        $findData = DB::table('rsview_ugdkasir')
-            ->select('datadaftarugd_json', 'vno_sep', 'reg_no')
-            ->where('rj_no', $rjno)
-            ->first();
-
-
-        $datadaftarugd_json = isset($findData->datadaftarugd_json) ? $findData->datadaftarugd_json : null;
-        // if meta_data_pasien_json = null
-        // then cari Data Pasien By Key Collection (exception when no data found)
-        //
-        // else json_decode
-        if ($datadaftarugd_json) {
-            $this->dataDaftarUgd = json_decode($findData->datadaftarugd_json, true);
-
-            // jika sep tidak ditemukan tambah variable sep pda array
-            if (isset($this->dataDaftarUgd['sep']) == false) {
-                $this->dataDaftarUgd['sep'] = [
-                    "noSep" => $findData->vno_sep,
-                    "reqSep" => [],
-                    "resSep" => [],
-                ];
-            }
-
-            // jika sep ditemukan tetapi variable kosong set sep pda array
-            $this->dataDaftarUgd['sep']['noSep'] = isset($findData->vno_sep) ? $findData->vno_sep : "";
-
-            // $this->emit('listenerRegNo', $this->dataDaftarUgd['regNo']);(nggak dipake)
-            $this->setDataPasien($this->dataDaftarUgd['regNo']);
-
-
-
-
-            if (!isset($this->dataDaftarUgd['klaimId'])) {
-                toastr()->closeOnHover(true)->closeDuration(3)->positionClass('toast-top-left')->addError("Data Klaim tidak ditemukan, Reset Data Ke UMUM");
-            }
-            if (!isset($this->dataDaftarUgd['kunjunganId'])) {
-                toastr()->closeOnHover(true)->closeDuration(3)->positionClass('toast-top-left')->addError("Data Kunjungan tidak ditemukan, Reset Data Ke FKTP");
-            }
-        } else {
-
-            toastr()->closeOnHover(true)->closeDuration(3)->positionClass('toast-top-left')->addError("Data tidak dapat di proses json.");
-            $dataDaftarUgd = DB::table('rsview_ugdkasir')
-                ->select(
-                    DB::raw("to_char(rj_date,'dd/mm/yyyy hh24:mi:ss') AS rj_date"),
-                    DB::raw("to_char(rj_date,'yyyymmddhh24miss') AS rj_date1"),
-                    'rj_no',
-                    'reg_no',
-                    'reg_name',
-                    'sex',
-                    'address',
-                    'thn',
-                    DB::raw("to_char(birth_date,'dd/mm/yyyy') AS birth_date"),
-                    'poli_id',
-                    // 'poli_desc',
-                    'dr_id',
-                    'dr_name',
-                    'klaim_id',
-                    'entry_id',
-                    'shift',
-                    'vno_sep',
-                    'no_antrian',
-
-                    'nobooking',
-                    'push_antrian_bpjs_status',
-                    'push_antrian_bpjs_json',
-                    // 'kd_dr_bpjs',
-                    // 'kd_poli_bpjs',
-                    'rj_status',
-                    'txn_status',
-                    'erm_status',
-                )
-                ->where('rj_no', '=', $rjno)
-                ->first();
-
-            $this->dataDaftarUgd = [
-                "regNo" => "" . $dataDaftarUgd->reg_no . "",
-
-                "drId" => "" . $dataDaftarUgd->dr_id . "",
-                "drDesc" => "" . $dataDaftarUgd->dr_name . "",
-
-                "poliId" => "" . $dataDaftarUgd->poli_id . "",
-
-                // "poliDesc" => "" . $dataDaftarUgd->poli_desc . "",
-
-                // "kddrbpjs" => "" . $dataDaftarUgd->kd_dr_bpjs . "",
-                // "kdpolibpjs" => "" . $dataDaftarUgd->kd_poli_bpjs . "",
-                "klaimId" => $dataDaftarUgd->klaim_id,
-                "rjDate" => "" . $dataDaftarUgd->rj_date . "",
-                "rjNo" => "" . $dataDaftarUgd->rj_no . "",
-                "shift" => "" . $dataDaftarUgd->shift . "",
-                "noAntrian" => "" . $dataDaftarUgd->no_antrian . "",
-                "noBooking" => "" . $dataDaftarUgd->nobooking . "",
-                "slCodeFrom" => "02",
-                "passStatus" => "",
-                "rjStatus" => "" . $dataDaftarUgd->rj_status . "",
-                "txnStatus" => "" . $dataDaftarUgd->txn_status . "",
-                "ermStatus" => "" . $dataDaftarUgd->erm_status . "",
-                "cekLab" => "0",
-                "kunjunganInternalStatus" => "0",
-                "noReferensi" => "" . $dataDaftarUgd->reg_no . "",
-                "postInap" => [],
-                "internal12" => "1",
-                "internal12Desc" => "Faskes Tingkat 1",
-                "internal12Options" => [
-                    [
-                        "internal12" => "1",
-                        "internal12Desc" => "Faskes Tingkat 1"
-                    ],
-                    [
-                        "internal12" => "2",
-                        "internal12Desc" => "Faskes Tingkat 2 RS"
-                    ]
-                ],
-                "kontrol12" => "1",
-                "kontrol12Desc" => "Faskes Tingkat 1",
-                "kontrol12Options" => [
-                    [
-                        "kontrol12" => "1",
-                        "kontrol12Desc" => "Faskes Tingkat 1"
-                    ],
-                    [
-                        "kontrol12" => "2",
-                        "kontrol12Desc" => "Faskes Tingkat 2 RS"
-                    ],
-                ],
-                "taskIdPelayanan" => [
-                    "taskId1" => "",
-                    "taskId2" => "",
-                    "taskId3" => "" . $dataDaftarUgd->rj_date . "",
-                    "taskId4" => "",
-                    "taskId5" => "",
-                    "taskId6" => "",
-                    "taskId7" => "",
-                    "taskId99" => "",
-                ],
-                'sep' => [
-                    "noSep" => "" . $dataDaftarUgd->vno_sep . "",
-                    "reqSep" => [],
-                    "resSep" => [],
-                ]
-            ];
-
-
-            // $this->emit('listenerRegNo', $this->dataDaftarUgd['regNo']);(nggak dipake)
-            $this->setDataPasien($this->dataDaftarUgd['regNo']);
-        }
-
-
-        //
-
-        // return $findData;
+        $this->dataDaftarUgd = $this->findDataUGD($rjno);
+        $this->setDataPasien($this->dataDaftarUgd['regNo']);
     }
 
 
