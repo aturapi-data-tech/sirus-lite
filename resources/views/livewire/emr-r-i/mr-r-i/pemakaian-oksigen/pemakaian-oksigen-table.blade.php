fd50a26faea69f6bd51cfb74f6fdbcc41eaf8baa
SYAMS. info <nuurwahidanshary@gmail.com>
2025-02-13 08:25:04 +0700

    fix(PemakaianOksigen):pemakaian Oksigen OK/ menghitung model /jenis alat dan durasi try1

--- resources/views/livewire/emr-r-i/mr-r-i/pemakaian-oksigen/pemakaian-oksigen-table.blade.php
+++ resources/views/livewire/emr-r-i/mr-r-i/pemakaian-oksigen/pemakaian-oksigen-table.blade.php
@@ -4,8 +4,7 @@
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="text-center">Tanggal / Jam </th>
-                    <th scope="col" class="text-center">Model Penggunaan - Durasi Penggunaan</th>
-                    <th scope="col" class="text-center">Jenis Alat Oksigen</th>
+                    <th scope="col" class="text-center">Keterangan</th>
                     <th scope="col" class="text-center">Dosis Oksigen</th>
                     <th scope="col" class="text-center">Action</th>
                 </tr>
@@ -36,7 +35,7 @@
                                 </div>
                             @else
                                 {{-- {{ $pemakaian['tanggalWaktuSelesai'] }} --}}
-                                <div class="flex flex-row justify-center">
+                                <div class="flex flex-row justify-center my-2">
                                     <div>
                                         Selesai :
                                     </div>
@@ -47,41 +46,40 @@
                                         value="{{ $pemakaian['tanggalWaktuSelesai'] }}" x-data
                                         x-on:keydown.enter="$wire.updateTanggalWaktuSelesai({{ $key }}, '{{ $pemakaian['tanggalWaktuMulai'] }}', $event.target.value)" />
                                 </div>
+                            @endif
+                        </td>
+
+                        <td class="text-center group-hover:bg-gray-100 group-hover:text-primary">
+
+                            Model :{{ $pemakaian['modelPenggunaan'] }}
+                            </br>
+                            Jenis Alat :{{ $pemakaian['jenisAlatOksigen'] }}
+                            @if ($pemakaian['jenisAlatOksigen'] === 'Lainnya')
+                                <br> <span
+                                    class="text-xs text-gray-500">({{ $pemakaian['jenisAlatOksigenDetail'] }})</span>
+                            @endif
+                            </br>
+                            Durasi :{{ $pemakaian['durasiPenggunaan'] }}
                         </td>
                         <td class="text-center group-hover:bg-gray-100 group-hover:text-primary">
-                @endif
-
-                {{ $pemakaian['modelPenggunaan'] }}
-                </br>
-                {{ $pemakaian['durasiPenggunaan'] }}
-
-
-
-                </td>
-                <td class="text-center group-hover:bg-gray-100 group-hover:text-primary">
-                    {{ $pemakaian['jenisAlatOksigen'] }}
-                    @if ($pemakaian['jenisAlatOksigen'] === 'Lainnya')
-                        <br> <span class="text-xs text-gray-500">({{ $pemakaian['jenisAlatOksigenDetail'] }})</span>
-                    @endif
-                </td>
-                <td class="text-center group-hover:bg-gray-100 group-hover:text-primary">
-                    {{ $pemakaian['dosisOksigen'] }}
-                    @if ($pemakaian['dosisOksigen'] === 'Lainnya')
-                        <br> <span class="text-xs text-gray-500">({{ $pemakaian['dosisOksigenDetail'] }})</span>
-                    @endif
-                </td>
-                <td class="text-center group-hover:bg-gray-100 group-hover:text-primary">
-                    <x-alternative-button class="inline-flex"
-                        wire:click.prevent="removePemakaianOksigen('{{ $pemakaian['tanggalWaktuMulai'] }}')">
-                        <svg class="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 18 20">
-                            <path
-                                d="M17 4h-4V2a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2H1a1 1 0 0 0 0 2h1v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1a1 1 0 1 0 0-2ZM7 2h4v2H7V2Zm1 14a1 1 0 1 1-2 0V8a1 1 0 0 1 2 0v8Zm4 0a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0v8Z" />
-                        </svg>
-                        Hapus
-                    </x-alternative-button>
-                </td>
-                </tr>
+                            {{ $pemakaian['dosisOksigen'] }}
+                            @if ($pemakaian['dosisOksigen'] === 'Lainnya')
+                                <br> <span
+                                    class="text-xs text-gray-500">({{ $pemakaian['dosisOksigenDetail'] }})</span>
+                            @endif
+                        </td>
+                        <td class="text-center group-hover:bg-gray-100 group-hover:text-primary">
+                            <x-alternative-button class="inline-flex"
+                                wire:click.prevent="removePemakaianOksigen('{{ $pemakaian['tanggalWaktuMulai'] }}')">
+                                <svg class="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 18 20">
+                                    <path
+                                        d="M17 4h-4V2a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2H1a1 1 0 0 0 0 2h1v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1a1 1 0 1 0 0-2ZM7 2h4v2H7V2Zm1 14a1 1 0 1 1-2 0V8a1 1 0 0 1 2 0v8Zm4 0a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0v8Z" />
+                                </svg>
+                                Hapus
+                            </x-alternative-button>
+                        </td>
+                    </tr>
                 @endforeach
 
             </tbody>
