c98f7fec7f9ba8c2eb92c053c977be08ccde28f0
SYAMS. info <nuurwahidanshary@gmail.com>
2025-03-27 11:57:37 +0700

    fix(CetakEresepRJ):tgl lahir if null ok

--- app/Http/Livewire/Cetak/CetakEresepRJ.php
+++ app/Http/Livewire/Cetak/CetakEresepRJ.php
@@ -310,7 +310,8 @@ class CetakEresepRJ extends Component
                 $this->dataPasien['pasien']['jenisKelamin']['jenisKelaminId'] = ($findData->sex == 'L') ? 1 : 2;
                 $this->dataPasien['pasien']['jenisKelamin']['jenisKelaminDesc'] = ($findData->sex == 'L') ? 'Laki-laki' : 'Perempuan';
                 $this->dataPasien['pasien']['tglLahir'] = $findData->birth_date ? $findData->birth_date : Carbon::now()->format('d/m/Y');
-                $this->dataPasien['pasien']['thn'] = Carbon::createFromFormat('d/m/Y', $findData->birth_date)->diff(Carbon::now(env('APP_TIMEZONE')))->format('%y Thn, %m Bln %d Hr'); //$findData->thn;
+
+                $this->dataPasien['pasien']['thn'] = Carbon::createFromFormat('d/m/Y', $this->dataPasien['pasien']['tglLahir'])->diff(Carbon::now(env('APP_TIMEZONE')))->format('%y Thn, %m Bln %d Hr'); //$findData->thn;
                 $this->dataPasien['pasien']['bln'] = $findData->bln;
                 $this->dataPasien['pasien']['hari'] = $findData->hari;
                 $this->dataPasien['pasien']['tempatLahir'] = $findData->birth_place;
